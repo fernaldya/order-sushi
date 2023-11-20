03fcfd34cebf285bb8a94013576a823caea9eed0
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2023-11-20 10:38:06 +0700

    feat: setup Container

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { Container } from "@/components/ui/container";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Container>
+    <h1 class="text-5xl font-semibold text-center text-primary">Order Sushi</h1>
+  </Container>
 </template>
 
 <style scoped>
