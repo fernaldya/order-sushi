aea0672477056e405f01a11761a04f1a864bf31b
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2023-11-20 11:16:21 +0700

    feat: setup vue-query

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,8 @@
+import { VueQueryPlugin } from "@tanstack/vue-query";
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
 import { router } from "./routes";
+import "./style.css";
 
 const app = createApp(App);
-app.use(router).mount("#app");
+app.use(router).use(VueQueryPlugin).mount("#app");
