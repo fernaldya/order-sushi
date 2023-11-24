5e9941fca041c295c7ee08009eafe6e03e7b0228
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2023-11-24 22:14:17 +0800

    feat: add share functionality

--- frontend/src/pages/Order.vue
+++ frontend/src/pages/Order.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Button } from "@/components/ui/button";
 import { Container } from "@/components/ui/container";
 import { Separator } from "@/components/ui/separator";
 import { useHead } from "@unhead/vue";
@@ -9,10 +10,19 @@ const route = useRoute();
 useHead({
   title: `Order #${route.params.id} | Order Sushi`,
 });
+
+const share = async () => {
+  await navigator.clipboard.writeText(window.location.href);
+};
 </script>
 
 <template>
   <Container>
+    <div class="flex justify-between mb-6">
+      <Button variant="outline" @click="share">Share</Button>
+      <Button>Finalize</Button>
+    </div>
+
     <h1 class="text-5xl text-center font-medium">
       Order #{{ $route.params.id }}
     </h1>
