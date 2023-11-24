5e32304f174fcfa84cbaabb0db29532f1cedd48c
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2023-11-24 22:03:59 +0800

    feat: setup head

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,10 @@
 import { VueQueryPlugin } from "@tanstack/vue-query";
+import { createHead } from "@unhead/vue";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./routes";
 import "./style.css";
 
 const app = createApp(App);
-app.use(router).use(VueQueryPlugin).mount("#app");
+const head = createHead();
+app.use(router).use(head).use(VueQueryPlugin).mount("#app");
